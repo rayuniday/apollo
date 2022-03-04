ff344b08b2df855ab7d1e73b575ec06715a478f5
Rayun <rayuniday@naver.com>
2022-03-04 19:22:41 +0900

    #0.4 Router and Styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Detail from "../routes/Detail";
+import Home from "../routes/Home";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/:id" element={<Detail />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
